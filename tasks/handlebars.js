fcd0e819dad2c9765de92201f4eaa7b85192fa70
Christopher Brown <io@henrian.com>
2013-05-16 13:44:42 -0500

    Little further up

--- tasks/handlebars.js
+++ tasks/handlebars.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
   grunt.registerMultiTask('handlebars', 'Precompile Handlebars template', function() {
     var done = this.async();
     var opts = this.data;
-    var handlebars = __dirname + '/node_modules/.bin/handlebars';
+    var handlebars = __dirname + '/node_modules/grunt-contrib-handlebars/node_modules/.bin/handlebars';
     // grunt.verbose.writeflags(opts, 'Options');
     glob(opts.src + '/*.' + (opts.ext || 'handlebars'), function (err, matches) {
       var args = [].concat(['-m'], matches, ['-f', opts.dest]);
