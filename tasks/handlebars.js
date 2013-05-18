dbd2ba7ce01dadff89ab923fde4f0d5c74885152
Christopher Brown <io@henrian.com>
2013-05-18 09:40:45 -0500

    Use handlebars script relative to require-found handlebars

--- tasks/handlebars.js
+++ tasks/handlebars.js
@@ -1,14 +1,17 @@
 var glob = require('glob');
+var path = require('path');
 
 module.exports = function(grunt) {
   grunt.registerMultiTask('handlebars', 'Precompile Handlebars template', function() {
     var done = this.async();
     var opts = this.data;
-    var handlebars = __dirname + '/../node_modules/.bin/handlebars';
+
+    var handlebars_js = require.resolve('handlebars');
+    var handlebars_bin = path.normalize(handlebars_js + '/../../bin/handlebars');
     grunt.verbose.writeflags(opts, 'Options');
     glob(opts.src + '/*.' + (opts.ext || 'handlebars'), function (err, matches) {
       var args = [].concat(['-m'], matches, ['-f', opts.dest]);
-      grunt.util.spawn({cmd: handlebars, args: args}, function (err, result, code) {
+      grunt.util.spawn({cmd: handlebars_bin, args: args}, function (err, result, code) {
         if (err) {
           grunt.fail.fatal(result);
         }
