845ffb61d4e4f0ab83fa93d97f48901b23b75ac0
Christopher Brown <io@henrian.com>
2013-06-09 17:12:10 -0500

    Resemble handlebars binary more closely.

--- tasks/handlebars.js
+++ tasks/handlebars.js
@@ -1,22 +1,28 @@
 var path = require('path');
 var glob = require('glob');
-
 module.exports = function(grunt) {
   grunt.registerMultiTask('handlebars', 'Precompile Handlebars template', function() {
     var done = this.async();
-    var opts = this.data;
-    var namespace = opts.namespace || 'Handlebars.templates';
-
+    var opts = grunt.util._.extend({templates: '**/*.mu'}, this.data);
     var handlebars_js = require.resolve('handlebars');
     var handlebars_bin = path.normalize(handlebars_js + '/../../bin/handlebars');
     grunt.verbose.writeflags(opts, 'Options');
-    glob(opts.glob, function (err, matches) {
-      var args = matches.concat(['--min', '--namespace', namespace, '--output', opts.dest]);
+    glob(opts.templates, function (err, args) {
+      if (opts.output)
+        args.push('--output', opts.output);
+      if (opts.minify)
+        args.push('--min');
+      if (opts.namespace)
+        args.push('--namespace', opts.namespace);
+      if (opts.root)
+        args.push('--root', opts.root);
+      if (opts.extension)
+        args.push('--extension', opts.extension);
       grunt.util.spawn({cmd: handlebars_bin, args: args}, function (err, result, code) {
         if (err) {
           grunt.fail.fatal(result);
         }
-        done();
+        done(err);
       });
     });
   });
